be20005a56e784b531f752c71347f92d8442f02d
unfif <age@sage.com>
2020-10-28 22:27:19 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Machi-Sta</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
+    <style>
+        html{
+            height: 100%;
+        }
+        body{
+            display: flex;
+            flex-direction: column;
+            min-height: 100%;
+            margin: 0;
+        }
+        header,
+        footer{
+            padding: 6px 20px;
+        }
+        header{
+            background: #333;
+            color: #eee;
+        }
+        footer{
+            display: flex;
+            justify-content: flex-end;
+            height: 18px;
+            background: #eee;
+            /* border-top: 2px solid; */
+        }
+        h1{
+            margin: 0;
+        }
+        main{
+            flex: 1;
+        }
+    </style>
+</head>
+<body>
+    <!-- <header>
+        <h1>Machi-Sta</h1>
+    </header> -->
+    <main>
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1oIkhBbj_yh9DD9tbXqT0p1lcOKIhNB3Q" width="100%" style="height: -webkit-fill-available"></iframe>
+    </main>
+    <!-- <footer>
+        <small>skmt</small>
+    </footer> -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+</body>
+</html>
